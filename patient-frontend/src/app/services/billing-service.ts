0253c5e56b3a2a5459dea5aee006081e06ba5456
Georgios Stratidis <stratidisgeorgios@gmail.com>
2026-06-28 19:35:11 +0200

    Add CORS config to API gateway

--- patient-frontend/src/app/services/billing-service.ts
+++ patient-frontend/src/app/services/billing-service.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { Inject, Injectable } from "@angular/core";
 import { BillingResponse, ChargeRequest } from "../models/billing.model";
 import { Observable } from "rxjs";
+import { APP_SERVICE_CONFIG, AppConfig } from "../app-config.interface";
 
 @Injectable({
   providedIn: "root",
 })
 export class BillingService {
-  constructor(private http: HttpClient) {}
+  constructor(@Inject(APP_SERVICE_CONFIG) private config: AppConfig, private http: HttpClient) {}
 
   getBillingInfo(patientId: string): Observable<BillingResponse> {
-    return this.http.get<BillingResponse>(`/api/billing/${patientId}`);
+    return this.http.get<BillingResponse>(`${this.config.apiUrl}/api/billing/${patientId}`);
   }
 
   addCharge(patientId: string, charge: ChargeRequest): Observable<void> {
-    return this.http.post<void>(`/api/billing/${patientId}/charge`, charge);
+    return this.http.post<void>(`${this.config.apiUrl}/api/billing/${patientId}/charge`, charge);
   }
 }
--- patient-frontend/src/app/services/catalog-service.ts
+++ patient-frontend/src/app/services/catalog-service.ts
@@ -1,27 +1,28 @@
 import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { Inject, Injectable } from "@angular/core";
 import { TreatmentRequest, TreatmentResponse } from "../models/catalog.model";
 import { Observable } from "rxjs";
+import { APP_SERVICE_CONFIG, AppConfig } from "../app-config.interface";
 
 @Injectable({
   providedIn: "root",
 })
 export class CatalogService {
-  constructor(private http: HttpClient) {}
+  constructor(@Inject(APP_SERVICE_CONFIG) private config: AppConfig, private http: HttpClient) {}
 
   getCatalog(): Observable<TreatmentResponse[]> {
-    return this.http.get<TreatmentResponse[]>(`/api/catalog`);
+    return this.http.get<TreatmentResponse[]>(`${this.config.apiUrl}/api/catalog`);
   }
 
   createTreatment(treatment: TreatmentRequest): Observable<void> {
-    return this.http.post<void>(`/api/catalog`, treatment);
+    return this.http.post<void>(`${this.config.apiUrl}/api/catalog`, treatment);
   }
 
   updateTreatment(treatmentId: string, treatment: TreatmentRequest): Observable<void> {
-    return this.http.put<void>(`/api/catalog/${treatmentId}`, treatment);
+    return this.http.put<void>(`${this.config.apiUrl}/api/catalog/${treatmentId}`, treatment);
   }
 
   deleteTreatment(treatmentId: string): Observable<void> {
-    return this.http.delete<void>(`/api/catalog/${treatmentId}`);
+    return this.http.delete<void>(`${this.config.apiUrl}/api/catalog/${treatmentId}`);
   }
 }
