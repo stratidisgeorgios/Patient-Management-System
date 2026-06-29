8991c20b79d3d2ea6cf2f2dcc1e1fd34b36fcff9
Georgios Stratidis <stratidisgeorgios@gmail.com>
2026-06-29 17:52:49 +0200

    CORS handling

--- api-gateway/src/main/java/com/patientsystem/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/patientsystem/apigateway/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "https://patientsystem.me"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://patientsystem.me", "https://patient-management-system-3fz.pages.dev"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
